db24453b4ca933b30afcd0944142841c84dc306c
jhincapie197 <jhincapie197@gmail.com>
2025-06-03 09:50:55 -0500

    Se crean variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,72 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Reservas APP");
+        System.out.println("===============================\n===============================");
+
+        //Entradas del algortmo
+        Scanner entradaTeclado=new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+
+        String nombreUsuario;
+        String idUsuario;
+        String correoUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamento;
+        String telefonoUsuarios;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoDelEspacio;
+        String utenciliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte numeroBloque;
+        byte cantidadAsistentes;
+        byte edadUsuario;
+        int valorReserva;
+
+        boolean esAdministrador=false;
+        boolean confirmacionReserva=false;
+        boolean estaOcupado=false;
+        boolean estaEnMantenimiento=false;
+        
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
